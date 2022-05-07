331516b32f273150e4e0508048d00cc43d1b5696
JuliusHendrix <jlam.hendrix@gmail.com>
2022-05-07 21:00:53 +0200

    * moved code for straight trajectory

--- Main/straight.py
+++ Main/straight.py
@@ -6,6 +6,10 @@ import sys
 import os
 from pathlib import Path
 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import pygame
+
 # modules
 script_dir = os.path.dirname(os.path.abspath(__file__))
 project_root_dir = str(Path(script_dir).parents[0])
@@ -97,15 +101,57 @@ class Manager:
         self.robot.move(0)
 
 
+def straight(mode):
+    with PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.framerate = 24
+        rawCapture = PiRGBArray(camera, size=(640, 480))
+        time.sleep(0.5)
+
+        # initialize bouncebot
+        bb = BounceBot()
+        # initialize object detector
+        detector = ObjectDetector()
+        # initialize timer
+        timer = Timer()
+
+        try:
+            for i in range(2):
+                timer.start_timer()
+                bb.move(1)
+
+                for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+                    if timer.get_timer() < distance_to_time(200):
+                        bb.move(0)
+                        break
+                    if mode == 2:
+                        obj, direction = detector.check_for_object(frame=frame, distance_to_dodge=25, show_video=True)
+                        if obj is not None:
+                            print("OBJECT DETECTED!")
+                            timer.pause_timer()
+                            bb.move(0)
+                            bb.around_obstacle(direction=direction)
+                            timer.resume_timer()
+                if i == 0:
+                    print("U-TURN")
+                    bb.u_turn()
+        finally:
+            camera.close()
+            rawCapture.close()
+            pygame.quit()
+            bb.close()
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--challenge_num", help="specifies which challenge the code runs on \
-                            [1-no object avoidance/2-object avoidance]", default=1)
+    parser.add_argument("--challenge_num", help="specifies which challenge the code runs on [1/2/3/4]", default=1)
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
 
-    m = Manager(mode=args.challenge_num)
-    m.start()
+    # m = Manager(mode=args.challenge_num)
+    # m.start()
+
+    straight(args.challenge_num)
