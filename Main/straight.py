957f0b4433f243bc373969e86f26b10fac34c5f3
ernestvmo <ernestvmo@gmail.com>
2022-05-08 11:59:37 +0200

    removed obsolete Manager class

--- Main/straight.py
+++ Main/straight.py
@@ -21,86 +21,6 @@ from RobotControl.bouncebot_control import BounceBot
 from RobotControl.plot_distance_vs_time import distance_to_time
 
 
-class Manager:
-    def __init__(self, mode):
-        self.timer = Timer()
-        self.detector = ObjectDetector()
-        self.robot = BounceBot()
-
-        self.mode = mode
-
-    # def detect(self):
-    #     vcap = cv2.VideoCapture(0)
-    #
-    #     if vcap.isOpened():
-    #         feed = True
-    #     else:
-    #         feed = False
-    #
-    #     detector = ObjectDetector()
-    #     while feed:
-    #         feed, frame = vcap.read()
-    #         _, direction = detector.check_for_object(frame=frame, distance_to_dodge=25) # 25?
-    #         if direction is not None:
-    #             self.timer.pause_timer()
-
-    def avoid(self, direction):
-        self.robot.around_obstacle(direction=direction)
-
-    def start(self):
-        self.timer.start_timer()
-        self.robot.move(1)
-
-        vcap = cv2.VideoCapture(0)
-        if vcap.isOpened():
-            feed = True
-        else:
-            feed = False
-            raise ValueError("Could not connect to the camera.")
-
-        while self.timer.get_timer() < distance_to_time(200) and feed:
-            # object detection
-            if self.mode == 2:
-                feed, frame = vcap.read()
-                obj, avoid_direction = self.detector.check_for_object(frame=frame, distance_to_dodge=25,
-                                                                      show_video=True)
-                if obj is not None:
-                    self.robot.move(0)
-                    self.timer.pause_timer()
-
-                    self.avoid(avoid_direction)
-
-                    self.robot.move(1)
-                    self.timer.resume_timer()
-        self.robot.move(0)
-
-        # supposedly we are at point B
-        self.robot.u_turn()
-
-        # pyslam check ?
-
-        # return
-        self.timer.start_timer()
-        self.robot.move(1)
-
-        while self.timer.get_timer() < distance_to_time(200) and feed:
-            # object detection
-            if self.mode == 2:
-                feed, frame = vcap.read()
-                obj, avoid_direction = self.detector.check_for_object(frame=frame, distance_to_dodge=25,
-                                                                      show_video=True)
-                if obj is not None:
-                    self.robot.move(0)
-                    self.timer.pause_timer()
-
-                    self.avoid(avoid_direction)
-
-                    self.robot.move(1)
-                    self.timer.resume_timer()
-
-        self.robot.move(0)
-
-
 def straight(mode, rendering):
     with PiCamera() as camera:
         camera.resolution = (640, 480)
@@ -173,7 +93,7 @@ def straight(mode, rendering):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--challenge_num", help="specifies which challenge the code runs on [1/2/3/4]", default=1,
+    parser.add_argument("-c", "--challenge_num", help="specifies which challenge the code runs on [1/2]", default=1,
                         type=int)
     parser.add_argument("-r", "--rendering", help="render video?", default=1, type=int)
     return parser.parse_args()
